5640de22c9f7366a8f24dde5e889cc9c1613e0f3
jakov-kusan <no@email.com>
2024-04-27 19:23:22 +0200

    doing stuff

--- src/gameOfLife.py
+++ src/gameOfLife.py
@@ -1,12 +1,25 @@
-import pygame
 
-
-
+def run(board,boardGeometry,generationLimit):
+    for generation in range(generationLimit):
 def main():
     board={}
+
+
+
     # boardGeometry*boardGeometry=cell number
-    boardGeometry
-    for 
+    boardGeometry=10
+    
+    # stop on the 100th generation
+    generationLimit=100
+
+
+    for x in range(boardGeometry):
+        for y in range(boardGeometry):
+            # 0 if cell dead, 1 if alive
+            board[x,y]=0
+
+
+
 
     print(board)
 if __name__ == "__main__":
